7c489fa427207103e74b43f36ae6f06f8997c08c
Sasha Miller <sashazm85@gmail.com>
2019-03-20 13:53:39 -0700

    code refactoring

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -2,6 +2,11 @@
 const navToggle = document.querySelector('.js-main-nav-toggle');
 const menu = document.querySelector('.header-main__nav');
 const menuToggle = document.querySelector('.js-menu-toggle');
+
+// Class names.
+const classMainActive = 'header-main__nav--active';
+const classToggleActive = 'nav-toggle--active';
+
 // Event listener to change toggle state icon
 navToggle.addEventListener('click', toggleStatus);
 menuToggle.addEventListener('click', hideMenu)
@@ -12,26 +17,25 @@ function toggleStatus () {
   toggleMenu()
 }
 
-
 function toggleNavClass () {
-  if (navToggle.className.includes('nav-toggle--active')) {
-    navToggle.classList.remove('nav-toggle--active');
+  if (navToggle.className.includes(classToggleActive)) {
+    navToggle.classList.remove(classToggleActive);
   } else {
-    navToggle.classList.add('nav-toggle--active');
+    navToggle.classList.add(classToggleActive);
   }
 }
 
 // to slide the side menu open and closed
 function toggleMenu () {
-  if (menu.className.includes('header-main__nav--active')) {
-    menu.classList.remove('header-main__nav--active');
+  if (menu.className.includes(classMainActive)) {
+    menu.classList.remove(classMainActive);
   } else {
-    menu.classList.add('header-main__nav--active');
+    menu.classList.add(classMainActive);
   }
 }
 
 // Slides menu to the left out of the view
 function hideMenu() {
-  menu.classList.remove('header-main__nav--active');
-  navToggle.classList.remove('nav-toggle--active');
+  menu.classList.remove(classMainActive);
+  navToggle.classList.remove(classToggleActive);
 }
